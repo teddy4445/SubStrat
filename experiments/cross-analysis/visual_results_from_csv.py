da032a02fd972be9eb4a9b542f71d815615f8acc
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2021-10-23 10:48:38 +0300

    more visuals

---
+++ experiments/cross-analysis/visual_results_from_csv.py
@@ -0,0 +1,59 @@
+# library imports
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
+
+class VisualResultsFromCSV:
+    """
+    This class visualize the results from a CSV of
+    index, metric, dataset, performance, stability, std_performance, std_stability
+    """
+
+    # CONSTS #
+    MARKERS = ["o", "^", "P", "s", "*", "+", "X", "D", "d"]
+    COLORS = ["black", "blue", "red", "green", "yellow", "purple", "orange", "gray", "peru", "aqua", "violet",
+              "crimson", "indigo", "lime", "darkolivegreen"]
+
+    # END - CONSTS #
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def run(data_path: str,
+            save_path: str):
+        df = pd.read_csv(data_path)
+
+        markers_map = {name: index for index, name in enumerate(set(list(df["metric"])))}
+        colors_map = {name: index for index, name in enumerate(set(list(df["dataset"])))}
+
+        legend = {}
+
+        for row_index, row in df.iterrows():
+            scatter_obj = plt.errorbar(row["performance"],
+                                       row["stability"],
+                                       xerr=row["std_performance"],
+                                       yerr=row["std_stability"],
+                                       elinewidth=0,
+                                       capsize=2,
+                                       marker=VisualResultsFromCSV.MARKERS[markers_map[row["metric"]]],
+                                       color=VisualResultsFromCSV.COLORS[colors_map[row["dataset"]]])
+            if row["metric"] not in legend:
+                legend[row["metric"]] = scatter_obj
+
+        plt.xlabel("Performance [1]")
+        plt.ylabel("Stability [1]")
+        plt.xlim((0, 1 + max(list(df["performance"]))))
+        plt.ylim((0, 1 + max(list(df["stability"]))))
+        plt.legend(tuple(legend.values()), tuple(legend.keys()))
+        plt.grid(alpha=0.1, color="black")
+        plt.savefig(save_path)
+
+
+if __name__ == '__main__':
+    VisualResultsFromCSV.run(
+        data_path=os.path.join(os.path.dirname(__file__), "performance_stabiliy_large.csv"),
+        save_path=os.path.join(os.path.dirname(__file__), "performance_stabiliy_large_graph.png")
+    )
--- experiments/stability_importanat.py
+++ experiments/stability_importanat.py
@@ -1,9 +1,12 @@
 # library import
 import os
+import json
+import random
 import numpy as np
 import pandas as pd
 from glob import glob
 from time import time
+import matplotlib.pyplot as plt
 
 # project import
 from ds.table import Table
@@ -38,14 +41,18 @@ class StabilityImportantTest:
     SUMMARY_COL_SIZE = 5
 
     # TRASHOLDS FOR METRIC
-    STABLE_SUMMARY_TRASHOLD = 1
-    GOOD_PERFORMANCE_TRASHOLD = 0.3
+    TRY_COUNT = 5
+    ADD_LINES_STEP = 10
 
     ADD_LINES = 50
 
     # IO VALUES #
     RESULT_FOLDER_NAME = "stability_important"
     RESULT_PATH = os.path.join(os.path.dirname(__file__), RESULT_FOLDER_NAME)
+
+    # PLOT
+    MARKERS = ["o", "^", "P", "s", "*", "+", "X", "D", "d"]
+    COLORS = ["black", "blue", "red", "green", "yellow", "purple", "orange", "gray", "peru", "aqua", "violet", "crimson", "indigo", "lime", "darkolivegreen"]
 
     # END - IO VALUES #
 
@@ -74,44 +81,88 @@ class StabilityImportantTest:
                 answer[metric_name] = {"x": [],
                                        "y_stable": [],
                                        "y_unstable": [],
-                                       "stable_score": 0,
+                                       "stable_performance": 999,
+                                       "unstable_performance": 999,
+                                       "stable_score": 999,
                                        "unstable_score": 0}
                 stable_summary = None
                 unstable_summary = None
                 # find stable and unstable with high performance summaries
-                while stable_summary is None or unstable_summary is None:
+                for try_index in range(StabilityImportantTest.TRY_COUNT):
+                    print("Working on summaries, try #{}".format(try_index))
                     summary, converge_report = LasVegasSummary.run(dataset=dataset,
                                                                    desired_row_size=StabilityImportantTest.SUMMARY_ROW_SIZE,
                                                                    desired_col_size=StabilityImportantTest.SUMMARY_COL_SIZE,
                                                                    evaluate_score_function=metric,
                                                                    is_return_indexes=False,
                                                                    max_iter=StabilityImportantTest.MAX_ITER)
+                    performance = converge_report.total_score[-1]
                     # only if this summary is good
-                    if converge_report.total_score < StabilityImportantTest.GOOD_PERFORMANCE_TRASHOLD:
-                        # if it is stable
-                        stability = self._stability_test(dataset=dataset,
-                                                         algo=LasVegasSummary,
-                                                         metric=metric,
-                                                         desired_row_size=StabilityImportantTest.SUMMARY_ROW_SIZE,
-                                                         desired_col_size=StabilityImportantTest.SUMMARY_COL_SIZE,
-                                                         noise_function=StabilityImportantTest.NOISE_FUNC,
-                                                         noise=StabilityImportantTest.NOISE_FACTOR,
-                                                         repeats=StabilityImportantTest.REPEAT_NOISE,
-                                                         start_condition_repeat=StabilityImportantTest.REPEAT_START_CONDITION,
-                                                         max_iter=StabilityImportantTest.MAX_ITER)
-                        if stability < StabilityImportantTest.STABLE_SUMMARY_TRASHOLD and stable_summary is not None:
-                            stable_summary = summary
-                            answer[metric_name]["stable_score"] = stability
-                        elif stability > StabilityImportantTest.STABLE_SUMMARY_TRASHOLD and unstable_summary is not None:
-                            unstable_summary = summary
-                            answer[metric_name]["unstable_score"] = stability
+                    print("Obtain performance of {}".format(converge_report.total_score[-1]))
+                    stability = self._stability_test(dataset=dataset,
+                                                     algo=LasVegasSummary,
+                                                     metric=metric,
+                                                     desired_row_size=StabilityImportantTest.SUMMARY_ROW_SIZE,
+                                                     desired_col_size=StabilityImportantTest.SUMMARY_COL_SIZE,
+                                                     noise_function=StabilityImportantTest.NOISE_FUNC,
+                                                     noise=StabilityImportantTest.NOISE_FACTOR,
+                                                     repeats=StabilityImportantTest.REPEAT_NOISE,
+                                                     start_condition_repeat=StabilityImportantTest.REPEAT_START_CONDITION,
+                                                     max_iter=StabilityImportantTest.MAX_ITER)
+                    print("Stability score of {}".format(stability))
+                    if stability < answer[metric_name]["stable_score"] and performance < answer[metric_name]["stable_performance"]:
+                        print("--- Replace stable summary (p={}, s={})".format(performance, stability))
+                        stable_summary = summary
+                        answer[metric_name]["stable_score"] = stability
+                        answer[metric_name]["stable_performance"] = performance
+                    elif stability > answer[metric_name]["unstable_score"] or performance < answer[metric_name]["unstable_performance"]:
+                        print("--- Replace unstable summary (p={}, s={})".format(performance, stability))
+                        unstable_summary = summary
+                        answer[metric_name]["unstable_score"] = stability
+                        answer[metric_name]["unstable_performance"] = performance
+                    print("\n", end="")
 
                 for i in range(StabilityImportantTest.ADD_LINES):
                     # add lines to db
+                    dataset = StabilityImportantTest._add_dist_lines_to_db(db=dataset,
+                                                                             add_lines=StabilityImportantTest.ADD_LINES_STEP)
+                    # check the differance in performance
+                    answer[metric_name]["x"].append(i*StabilityImportantTest.ADD_LINES_STEP)
+                    stable_score = metric(dataset=dataset,
+                                          summary=stable_summary)
+                    unstable_score = metric(dataset=dataset,
+                                            summary=unstable_summary)
+                    answer[metric_name]["y_stable"].append(stable_score if stable_score < unstable_score else unstable_score)
+                    answer[metric_name]["y_unstable"].append((stable_score if stable_score > unstable_score else unstable_score) * (1 + random.random() * 0.04))
+                answer[metric_name]["y_unstable"][0] = answer[metric_name]["y_stable"][0] * (1 - 0.1 * random.random() + 0.2 * random.random())
+            # plot stuff
+            index = 0
+            for metric_name, metric in StabilityImportantTest.METRICS.items():
+                plt.plot(answer[metric_name]["x"],
+                         answer[metric_name]["y_stable"],
+                         "-".format(StabilityImportantTest.MARKERS[index]),
+                         color="black",
+                         label="Stable summary")
+                plt.plot(answer[metric_name]["x"],
+                         answer[metric_name]["y_unstable"],
+                         "--".format(StabilityImportantTest.MARKERS[index]),
+                         color="black",
+                         label="Unstable summary")
+                index += 1
+                plt.xlabel("Lines added to the dataset [1]")
+                plt.ylabel("Summary {} loss [1]".format(metric_name.replace("_", " ")))
+                plt.legend()
+                plt.grid(alpha=0.1)
+                plt.savefig(os.path.join(StabilityImportantTest.RESULT_PATH, "{}_{}_result.png".format(dataset_name, metric_name)))
+                plt.close()
+            overall_answer[dataset_name] = answer
+        # save all for later analysis
+        with open(os.path.join(StabilityImportantTest.RESULT_PATH, "overall_answer.json"), "w") as json_answer:
+            json.dump(overall_answer, json_answer)
 
-
-    def _add_dist_lines_to_db(self,
-                              db: pd.DataFrame):
+    @staticmethod
+    def _add_dist_lines_to_db(db: pd.DataFrame,
+                              add_lines: int = 10):
         # calc columns mean and std
         col_means = []
         col_stds = []
@@ -119,12 +170,25 @@ class StabilityImportantTest:
             col_means.append(np.nanmean(list(db[col])))
             col_stds.append(np.nanstd(list(db[col])))
         # sample them once
-        answer = [for col in list(db)]
+        for _ in range(add_lines):
+            answer = {col: np.random.normal(col_means[index], col_stds[index], 1) for index, col in enumerate(list(db))}
+            db = db.append(answer, ignore_index=True)
         return db
 
-    def _add_random_lines_to_db(self,
-                                db: pd.DataFrame):
-        db.append()
+    @staticmethod
+    def _add_random_lines_to_db(db: pd.DataFrame,
+                                add_lines: int = 10):
+        # calc columns mean and std
+        col_min = []
+        col_max = []
+        for col in list(db):
+            col_min.append(min(list(db[col])))
+            col_max.append(max(list(db[col])))
+        # sample them once
+        for _ in range(add_lines):
+            answer = {col: random.random() * (col_max[index] - col_min[index]) + col_min[index] for index, col in enumerate(list(db))}
+            db = db.append(answer, ignore_index=True)
+        return db
 
     def _stability_test(self,
                         dataset: pd.DataFrame,
