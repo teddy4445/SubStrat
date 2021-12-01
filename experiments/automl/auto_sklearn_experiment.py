29f2cbb2a62a774b289752c31c5a70a7f2d49428
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2021-12-01 18:13:08 +0200

    start automl exp

---
+++ experiments/automl/auto_sklearn_experiment.py
@@ -0,0 +1,19 @@
+# library imports
+
+# project imports
+
+
+class AutoSKlearnExperiment:
+    """
+    An experiments to use the stable sub-table in the usage of auto ML experiment
+    """
+
+    # CONSTS #
+
+    # END - CONSTS #
+
+    def __init__(self):
+        pass
+
+
+
