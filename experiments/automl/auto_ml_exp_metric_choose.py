5767923ce93cfe519ee06298f95c76347afc6770
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2021-12-08 11:57:49 +0200

    better auto-ml exp

--- experiments/automl/auto_ml_exp_metric_choose.py
+++ experiments/automl/auto_ml_exp_metric_choose.py
@@ -19,10 +19,6 @@ from sklearn.model_selection import train_test_split
 from pandas.core.dtypes.common import is_numeric_dtype
 
 # project imports
-from ds.table import Table
-from experiments.stability_experiment import prepare_dataset
-from methods.summary_wellness_scores import SummaryWellnessScores
-from ds.dataset_properties_measurements import DatasetPropertiesMeasurements
 from summary_algorithms.stabler_genetic_algorithm_summary_algorithm import StablerGeneticSummary
 
 
@@ -30,63 +26,18 @@ from summary_algorithms.stabler_genetic_algorithm_summary_algorithm import Stabl
 random.seed(73)
 
 
-def prepare_dataset_full(df):
-    # remove what we do not need
-    df.drop([col for col in list(df) if not is_numeric_dtype(df[col])], axis=1, inplace=True)
-    # remove _rows with nan
-    df.dropna(inplace=True)
-    # get only max number of _rows to work with
-    return df
-
-
-class AutoSKlearnExperiment:
+class AutoSKlearnExperimentMetricChoose:
     """
-    An experiments to use the stable sub-table in the usage of auto ML experiment
-
-    Cite:
-    @inproceedings{feurer-neurips15a,
-        title     = {Efficient and Robust Automated Machine Learning},
-        author    = {Feurer, Matthias and Klein, Aaron and Eggensperger, Katharina  Springenberg, Jost and Blum, Manuel and Hutter, Frank},
-        booktitle = {Advances in Neural Information Processing Systems 28 (2015)},
-        pages     = {2962--2970},
-        year      = {2015}
-    }
-
+    Continue the experiment at '/experiments/automl/auto_sklearn_experiment.py' to pick the best metric to work with
     """
 
-    # CONSTS #
-    DATASETS = {os.path.basename(path).replace(".csv", ""): prepare_dataset_full(pd.read_csv(path))
-                for path in glob(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "*.csv"))}
-
-    METRICS = {
-        "entropy": SummaryWellnessScores.mean_entropy,
-        "coefficient_of_anomaly": SummaryWellnessScores.coefficient_of_anomaly,
-        "coefficient_of_variation": SummaryWellnessScores.coefficient_of_variation,
-    }
+    METRICS = ["entropy", "coefficient_of_anomaly","coefficient_of_variation"]
 
     MARKERS = {
         "entropy": "o",
         "coefficient_of_anomaly": "X",
         "coefficient_of_variation": "^",
     }
-
-    COLORS = {os.path.basename(path).replace(".csv", ""): "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-                for path in glob(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "*.csv"))}
-
-    # STABILITY TEST FACTORS
-    NOISE_FUNC = SummaryWellnessScores.add_dataset_subset_pick_noise
-    NOISE_FACTOR = 0.05
-    REPEAT_NOISE = 3
-    REPEAT_START_CONDITION = 3
-    REPEAT_SUMMARY = 5
-
-    # ALGORITHM HYPER-PARAMETERS
-    MAX_ITER = 25
-    SUMMARY_ROW_SIZE = 20
-    SUMMARY_COL_SIZE = 5
-    NAIVE_TIMES = 10
-
-    COL_PORTION = 0.75
 
     # IO VALUES
     RESULT_FOLDER_NAME = "auto_ml_results"
@@ -102,14 +53,14 @@ class AutoSKlearnExperiment:
             test_portion: float = 0.1):
         # make sure we have the folder for the answers
         try:
-            os.mkdir(AutoSKlearnExperiment.RESULT_PATH)
+            os.mkdir(AutoSKlearnExperimentMetricChoose.RESULT_PATH)
         except Exception as error:
             pass
 
         answer = {}
-        for dataset_name, dataset in AutoSKlearnExperiment.DATASETS.items():
+        for dataset_name, dataset in AutoSKlearnExperimentMetricChoose.DATASETS.items():
             answer[dataset_name] = {}
-            for metric_name, metric in AutoSKlearnExperiment.METRICS.items():
+            for metric_name, metric in AutoSKlearnExperimentMetricChoose.METRICS.items():
                 try:
                     print("Start: {} with {}".format(dataset_name, metric_name))
                     # split data
@@ -127,49 +78,43 @@ class AutoSKlearnExperiment:
                     full_data_acc = accuracy_score(predictions, y_test)
                     full_data_end = time()
 
-                    naive_time_list = []
-                    naive_acc_list = []
+                    # naive baseline #
+                    naive_rows = random.sample(list(range(x_train.shape[0])), round(math.sqrt(dataset.shape[0])))
+                    naive_columns = random.sample(list(range(x_train.shape[1])), round(AutoSKlearnExperimentMetricChoose.COL_PORTION * dataset.shape[1]))
+                    naive_columns_full = naive_columns.copy()
+                    # get the summary
+                    if dataset.shape[1] - 1 not in naive_columns_full:
+                        naive_columns_full.append(dataset.shape[1] - 1)
+                    summary = dataset.iloc[naive_rows, naive_columns_full]
 
-                    for _ in range(AutoSKlearnExperiment.NAIVE_TIMES):
-                        # naive baseline #
-                        naive_rows = random.sample(list(range(x_train.shape[0])), round(math.sqrt(dataset.shape[0])))
-                        naive_columns = random.sample(list(range(x_train.shape[1])), round(AutoSKlearnExperiment.COL_PORTION * dataset.shape[1]))
-                        naive_columns_full = naive_columns.copy()
-                        # get the summary
-                        if dataset.shape[1] - 1 not in naive_columns_full:
-                            naive_columns_full.append(dataset.shape[1] - 1)
-                        summary = dataset.iloc[naive_rows, naive_columns_full]
+                    # sub-table learning
+                    x_summary, y_summary = summary.drop([target_feature_name], axis=1), summary[target_feature_name]
+                    x_train_summary, x_test_summary, y_train_summary, y_test_summary = train_test_split(x_summary, y_summary, test_size=test_portion, random_state=73)
 
-                        # sub-table learning
-                        x_summary, y_summary = summary.drop([target_feature_name], axis=1), summary[target_feature_name]
-                        x_train_summary, x_test_summary, y_train_summary, y_test_summary = train_test_split(x_summary, y_summary, test_size=test_portion, random_state=73)
-
-                        naive_start = time()
-                        try:
-                            cls = autosklearn.classification.AutoSklearnClassifier()
-                        except Exception as error:
-                            cls = DecisionTreeClassifier()
-                        cls.fit(x_train_summary, y_train_summary)
-                        try:
-                            predictions = cls.predict(x_test.iloc[:, naive_columns])
-                            naive_acc = accuracy_score(predictions, y_test)
-                        except Exception as error:
-                            predictions = cls.predict(x_test_summary)
-                            naive_acc = accuracy_score(predictions, y_test_summary)
-                        naive_end = time()
-                        naive_time_list.append((naive_end-naive_start)*11)
-                        naive_acc_list.append(naive_acc)
+                    naive_start = time()
+                    try:
+                        cls = autosklearn.classification.AutoSklearnClassifier()
+                    except Exception as error:
+                        cls = DecisionTreeClassifier()
+                    cls.fit(x_train_summary, y_train_summary)
+                    try:
+                        predictions = cls.predict(x_test.iloc[:, naive_columns])
+                        naive_acc = accuracy_score(predictions, y_test)
+                    except Exception as error:
+                        predictions = cls.predict(x_test_summary)
+                        naive_acc = accuracy_score(predictions, y_test_summary)
+                    naive_end = time()
 
                     # get sub-table (summary)
                     best_rows, best_columns, converge_report = StablerGeneticSummary.run(dataset=x_train,
                                                                                          desired_row_size=round(
                                                                                              math.sqrt(dataset.shape[0])),
                                                                                          desired_col_size=round(
-                                                                                             AutoSKlearnExperiment.COL_PORTION *
+                                                                                             AutoSKlearnExperimentMetricChoose.COL_PORTION *
                                                                                              dataset.shape[1]),
                                                                                          evaluate_score_function=metric,
                                                                                          is_return_indexes=True,
-                                                                                         max_iter=AutoSKlearnExperiment.MAX_ITER)
+                                                                                         max_iter=AutoSKlearnExperimentMetricChoose.MAX_ITER)
                     # get the summary
                     best_columns_x = best_columns.copy()
                     if dataset.shape[1] - 1 not in best_columns:
@@ -197,13 +142,13 @@ class AutoSKlearnExperiment:
                     # compute answer and save it
                     answer[dataset_name][metric_name] = ((full_data_end - full_data_start) * 50,
                                                          (summary_end - summary_start) * 11,
-                                                         np.mean(naive_time_list),
+                                                         (naive_end - naive_start) * 11,
                                                          full_data_acc,
                                                          summary_acc,
-                                                         np.mean(naive_acc_list))
+                                                         naive_acc)
 
                     # safe the results at each point we need
-                    with open(os.path.join(AutoSKlearnExperiment.RESULT_PATH, "raw_data.json"), "w") as answer_file_json:
+                    with open(os.path.join(AutoSKlearnExperimentMetricChoose.RESULT_PATH, "raw_data.json"), "w") as answer_file_json:
                         json.dump(answer,
                                   answer_file_json,
                                   indent=2)
@@ -211,7 +156,7 @@ class AutoSKlearnExperiment:
                     print("Skipping this dataset {}".format(error))
 
         # safe the results at each point we need
-        with open(os.path.join(AutoSKlearnExperiment.RESULT_PATH, "raw_data.csv"), "w") as answer_file_csv:
+        with open(os.path.join(AutoSKlearnExperimentMetricChoose.RESULT_PATH, "raw_data.csv"), "w") as answer_file_csv:
             csv_answer = "dataset,metric,full_time_sec,subtable_time_sec,baseline_time_sec,full_accuracy,subtable_accuracy,baseline_accuracy\n"
             for dataset in answer.keys():
                 for metric in answer[dataset].keys():
@@ -237,16 +182,16 @@ class AutoSKlearnExperiment:
         for val in nice_answer:
             plt.scatter(val[2],
                         val[3],
-                        color=AutoSKlearnExperiment.COLORS[val[0]],
-                        marker=AutoSKlearnExperiment.MARKERS[val[1]])
+                        color=AutoSKlearnExperimentMetricChoose.COLORS[val[0]],
+                        marker=AutoSKlearnExperimentMetricChoose.MARKERS[val[1]])
         plt.xlabel("Relative change in computation time [t]")
         plt.ylabel("Relative change in performance [1]")
         plt.xlim((0, 1))
         plt.ylim((-0.2, 1))
         plt.grid(alpha=0.2, color="black")
-        plt.savefig(os.path.join(AutoSKlearnExperiment.RESULT_PATH, "results.png"))
+        plt.savefig(os.path.join(AutoSKlearnExperimentMetricChoose.RESULT_PATH, "results.png"))
         plt.close()
 
 
 if __name__ == '__main__':
-    AutoSKlearnExperiment.run()
+    AutoSKlearnExperimentMetricChoose.run()
